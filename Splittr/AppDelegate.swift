187ce2641f500e47b5e6597d2cf5c7395f4de9fe
Jack Wu <masterjack1997@gmail.com>
2020-03-24 23:19:47 -0400

    pricefield to decimalpad, add iqkeyboardmanager

--- Splittr/AppDelegate.swift
+++ Splittr/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import RealmSwift
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,12 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        IQKeyboardManager.shared.enable = true
         //print(Realm.Configuration.defaultConfiguration.fileURL)
-        do {
-            _ = try Realm()
-        } catch {
-            print("Error initializing new realm, \(error)")
-        }
+//        do {
+//            _ = try Realm()
+//        } catch {
+//            print("Error initializing new realm, \(error)")
+//        }
         return true
     }
 
